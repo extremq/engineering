b772ee73fc80e9d9f382583756b6e63b075e13e3
extremq <45830561+extremq@users.noreply.github.com>
2021-11-26 23:37:38 +0200

    Add test.

--- PC - First year/l8/main.c
+++ PC - First year/l8/main.c
@@ -1,23 +1,65 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "geometrie.h"
+#include <stdlib.h>
+#include "parcare.h"
+
+// Stergem din uratenia if-urilor.
+void checkMemory(unsigned int* p);
+void showResults(unsigned int* l, unsigned int* p, unsigned int* r, int n);
+
+void checkMemory(unsigned int* p)
+{
+	if(p == 0)
+	{
+		fprintf(stderr, "Not enough memory for array.");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void showResults(unsigned int* locuri, unsigned int* preturi, unsigned int* sume, int n)
+{
+	readArray(locuri, n);
+	readArray(preturi, n);
+
+	printf("Primaria gestioneaza %d parcari.\n\n", n);
+	printf("Numarul de locuri de parcare este: ");
+	printArray(locuri, n);
+	printf(".\n");
+
+	printf("Pretul pentru un loc de parcare este: ");
+	printArray(preturi, n);
+	printf(" lei.\n\n");
+	
+	sumeIncasate(locuri, preturi, sume, n);	
+
+	printf("Sumele incasate sunt: ");
+	printArray(sume, n);
+	printf(" lei.\n\n");
+
+	int indice = locuriMax(locuri, n);	
+	printf("Parcarea cu cele mai multe locuri de parcare este parcarea %d.\n", indice);
+	printf("Pretul unui loc in parcarea %d este de %d lei.\n", indice, *(preturi + indice));
+}
 
 int main(void)
 {
 	int n;
+	unsigned int *locuri = 0, *preturi = 0;
+	unsigned int *sume = 0;
 
 	scanf("%d", &n);
+
+	locuri = (unsigned int*) malloc(n * sizeof(unsigned int));
+	checkMemory(locuri);
 	
-	punct* p = nPuncte(n);
+	preturi = (unsigned int*) malloc(n * sizeof(unsigned int));
+	checkMemory(locuri);
+	
+	sume = (unsigned int*) malloc(n * sizeof(unsigned int));
+	checkMemory(sume);	
 
-	int i;
-	for(i = 0; i < n; ++i)
-	{
-		citestePunct(p + i);
-		afiseazaPunct(p + i);
-	}
+	showResults(locuri, preturi, sume, n);
 
-	printf("%.4lf\n", distantaPuncte(p, p + 1));
-
+	free(locuri);
+	free(preturi);
 	return 0;	
 }

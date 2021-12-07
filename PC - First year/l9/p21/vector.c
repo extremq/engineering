be2734139db031491714fa81cc2f5ff54fc2f5e7
extremq <stefansteff999@gmail.com>
2021-12-07 20:47:03 +0200

    Add homework.

--- PC - First year/l9/p21/vector.c
+++ PC - First year/l9/p21/vector.c
@@ -1,17 +1,134 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
 
 #include "vector.h"
 
 void setName(vReal* T)
 {
-    
+    T -> name = (char*) malloc(sizeof(char) * MAX_NAME_LENGTH);
+    if(T -> name == 0)
+    {
+        fprintf(stderr, "Couldn't allocate memory for name buffer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Daca sirul e mai mare decat MAX_NAME_LENGTH, programul tot va scrie
+    // peste adrese. Aici ar trebui verificate lungimile.
+    scanf("%s", T -> name);
 }
-void setArray(vReal* T);
-void setVector(vReal* T);
 
-void printArray(vReal* T);
+void setArray(vReal* T, int len)
+{
+    T -> len = len;
 
-int getSortChoice();
-void sortArray(vReal* T, int option);
+    // Presupun ca sunt obligat sa impart citirea si pe main,
+    // si pe header.
+    //scanf("%d", &(T -> len));
 
-void swapDouble(double* a, double* b);
+    T -> arr = (double*) malloc(sizeof(double) * T -> len);
+    if(T -> arr == 0)
+    {
+        fprintf(stderr, "Couldn't allocate memory for array buffer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int i;
+    for(i = 0; i < T -> len; ++i)
+    {
+        scanf("%lf", T -> arr + i);
+    }
+}
+
+void printArray(vReal* T)
+{
+    int i;
+    printf("%s: [ ", T -> name);
+    for(i = 0; i < T -> len; ++i)
+    {
+        printf("%.3lf ", T -> arr[i]);
+    }
+    printf("]\n");
+}
+
+int getSortChoice()
+{
+    int sortChoice = 0;
+    scanf("%d", &sortChoice);
+
+    return sortChoice;
+}
+
+void sortArray(vReal* T, int option)
+{
+    _Bool finished = false;
+
+    int i, len = T -> len;
+    double* arr = T -> arr;
+
+    while(!finished)
+    {
+        finished = true;
+
+        for(i = 0; i < len - 1; ++i)
+        {
+            if(assertSwap(arr[i], arr[i + 1], option))
+            {
+                swapDouble(&arr[i], &arr[i + 1]);
+                finished = false;
+            }
+        }
+
+        --len;
+    } 
+}
+
+_Bool assertSwap(double a, double b, int option)
+{
+    if(option == ASCENDING_VALUE)
+    {
+        return a > b;
+    }
+    else if(option == ASCENDING_DIGIT_SUM)
+    {
+        // Practic, double poate avea valori imense
+        // fata de cat poate stoca int, dar presupun
+        // ca suntem cuminti de data aceasta si vom
+        // da cast.
+        return getDigitSum((long) a) > getDigitSum((long) b);   
+    }
+    else {
+        // Warning daca nu avem default return
+        return false;
+    }
+}
+
+int getDigitSum(long a)
+{
+    int sum = 0;
+    
+    // Nu mai importam math.h degeaba.
+    a = a < 0 ? -a : a;
+
+    while(a)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+
+    return sum;
+}
+
+void swapDouble(double* a, double* b)
+{
+    double aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void dealloc(vReal* T)
+{
+    free(T -> name);
+    free(T -> arr);
+}
--- PC - First year/l9/p21/vector.h
+++ PC - First year/l9/p21/vector.h
@@ -1,8 +1,12 @@
 #ifndef __VECTOR_H_
 #define __VECTOR_H_
 
+#define MAX_NAME_LENGTH 256
+#define ASCENDING_VALUE 1
+#define ASCENDING_DIGIT_SUM 2
+
 struct _VECTORR {
-    char* nume;
+    char* name;
     int len;
     double* arr;
 };
@@ -10,13 +14,14 @@ struct _VECTORR {
 typedef struct _VECTORR vReal;
 
 void setName(vReal* T);
-void setArray(vReal* T);
-void setVector(vReal* T);
+void setArray(vReal* T, int len);
 void printArray(vReal* T);
 
 int getSortChoice();
 void sortArray(vReal* T, int option);
 
+_Bool assertSwap(double a, double b, int option);
+int getDigitSum(long a);
 void swapDouble(double* a, double* b);
 
 void dealloc(vReal* T);

e2bcc446fd130bbb446f7df280845cf104847288
extremq <stefansteff999@gmail.com>
2022-03-17 20:32:31 +0200

    Add p2.

---
+++ CP/p2/integrale.h
@@ -0,0 +1,11 @@
+#ifndef INTEGRALE_H_
+#define INTEGRALE_H_
+
+typedef double (*PF)(double);
+
+double integralaTrapez(double a, double b, unsigned int n, PF pf);
+double integralaDreptunghi(double a, double b, unsigned int n, PF pf);
+double integralaSimpson(double a, double b, unsigned int n, PF pf);
+double f(double x);
+
+#endif

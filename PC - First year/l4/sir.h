a5d4981c14c3edf73ae0b74f1625f9835927d54c
extremq <45830561+extremq@users.noreply.github.com>
2022-03-17 21:51:15 +0200

    Delete PC - First year directory

--- PC - First year/l4/sir.h
+++
@@ -1,7 +0,0 @@
-#ifndef SIR_H_
-#define SIR_H_
-
-double termen(int n, int p, double a);
-double urmatorul_termen(double x, int p, double a);
-
-#endif
--- PC - First year/l5/divizori.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DIVIZORI_H_
-#define __DIVIZORI_H_
-
-int suma_divizori(int n);
-
-#endif

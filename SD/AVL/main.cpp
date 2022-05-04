6573907d8d6da8a48777271bf1872fc700cecd55
extremq <45830561+extremq@users.noreply.github.com>
2022-05-04 21:10:13 +0300

    Update main.cpp

--- SD/AVL/main.cpp
+++ SD/AVL/main.cpp
@@ -4,5 +4,17 @@
 int main() {
     AVL<int> tree;
 
-    tree.insert(1);
+    tree.insert(9);
+    tree.insert(7);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(6);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(15);
+    tree.insert(11);
+    tree.print();
+    tree.indent();
 }

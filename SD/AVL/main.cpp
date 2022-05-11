44fa147b95c1f31200eca6f6329ab7178e4dcca5
extremq <stefansteff999@gmail.com>
2022-05-11 13:28:32 +0300

    Add some clarity.

--- SD/AVL/main.cpp
+++ SD/AVL/main.cpp
@@ -4,17 +4,15 @@
 int main() {
     AVL<int> tree;
 
-    tree.insert(9);
-    tree.insert(7);
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(6);
-    tree.insert(8);
-    tree.insert(10);
-    tree.insert(12);
-    tree.insert(14);
-    tree.insert(15);
-    tree.insert(11);
-    tree.print();
-    tree.indent();
+    int n;
+    std::cin >> n;
+    while(n--) {
+        int x;
+        std::cin >> x;
+        std::cout << "-------\n";
+        tree.insert(x);
+        tree.indent();
+        std::cout << "-------\n";
+        
+    }
 }
